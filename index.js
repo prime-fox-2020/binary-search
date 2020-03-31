7b06994fe1e0e9f0bcf05090d69a0b9546d28af6
riolly <albert.r.cahyantara@gmail.com>
2020-03-31 20:17:08 +0700

    Release 1: Binary search

--- index.js
+++ index.js
@@ -19,23 +19,40 @@ function ownSort(arr) {
 }
 
 function binary_search (search, array) {
+  let begin = 0;
+  let mid = Math.floor(array.length/2);
+  let end = array.length - 1;
+  let findFlag = false;
+  do {
+    if (begin == mid && mid == end) {
+      findFlag = true;
+    }
+    if (search === array[mid]) {
+      return mid;
+    } else if (search < array[mid]) { 
+      end = mid - 1;
+      mid = Math.floor(end/2);
+    } else if (search > array[mid]) {
+      begin = mid + 1;
+      mid = begin + Math.floor((end-begin)/2);
+    }
+  }
+  while (!findFlag);
   return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap);
 var arrayGanjilSorted = ownSort(testArrayGanjil);
-console.log(arrayGenapSorted);
-console.log(arrayGanjilSorted);
+// console.log(arrayGenapSorted);
+// console.log(arrayGanjilSorted);
 
 // // Driver code
-// console.log(arrayGenapSorted);
-// console.log(binary_search(8, arrayGenapSorted))
-// console.log(binary_search(10, arrayGenapSorted))
-// console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(10, arrayGenapSorted))
+console.log(binary_search(33, arrayGenapSorted))
 
-// console.log(arrayGanjilSorted);
-// console.log(binary_search(53, arrayGanjilSorted))
-// console.log(binary_search(3, arrayGanjilSorted))
-// console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted))
+console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(2, arrayGanjilSorted))
 
 module.exports = binary_search
