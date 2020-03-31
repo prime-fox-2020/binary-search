6ad6ed0d74393f5dee39e43da3fc32b4540634ed
akmaliavitri <akmaliavitri21@gmail.com>
2020-03-31 19:03:01 +0700

    search

--- index.js
+++ index.js
@@ -33,10 +33,25 @@ function ownSort(arr) {
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  
-  return 0;
+  let left = 0
+  let right = array.length-1
+
+  while(right >= left){
+    let mid = Math.round((right + left) / 2)
+
+    if(search < array[mid]){
+      right = mid-1
+    } else if(search > array[mid]){
+      left = mid+1
+    }
+
+    if(search === array[mid]){
+      return mid
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -45,12 +60,12 @@ console.log(arrayGenapSorted)
 console.log(arrayGanjilSorted)
 
 // Driver code
-// console.log(binary_search(8, arrayGenapSorted))
-// console.log(binary_search(10, arrayGenapSorted))
-// console.log(binary_search(33, arrayGenapSorted))
+console.log(binary_search(8, arrayGenapSorted))
+console.log(binary_search(10, arrayGenapSorted))
+console.log(binary_search(33, arrayGenapSorted))
 
-// console.log(binary_search(53, arrayGanjilSorted))
-// console.log(binary_search(3, arrayGanjilSorted))
-// console.log(binary_search(2, arrayGanjilSorted))
+console.log(binary_search(53, arrayGanjilSorted))
+console.log(binary_search(3, arrayGanjilSorted))
+console.log(binary_search(2, arrayGanjilSorted))
 
-//module.exports = binary_search
+module.exports = binary_search
