29323c48500a091ec04d4857a8493b111b21d2f8
issdq <ismail.ashshidiq@gmail.com>
2020-03-31 21:33:01 +0700

    ini gatau error kenapa, klo pake test case lain bisa

--- index.js
+++ index.js
@@ -5,16 +5,49 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j]
+      j = j - 1
+    }
+    arr[j + 1] = key
+  }
+
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, arr, left, right) {
   // Your searching code
-  return 0;
+  left = left || 0
+  right = right || arr.length - 1
+
+  if (right >= left) {
+    let mid = Math.floor((left + right) / 2)
+
+    if (arr[mid] === search)
+      return mid;
+
+    if (arr[mid] > search)
+      return binary_search(search, arr, left, mid - 1)
+
+    return binary_search(search, arr, mid + 1, right)
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+
+//Maaf bingung errornya dimana, klo pake test di bawah ini bisa
+console.log(binary_search(7, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])); //6
+console.log(binary_search(11, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])); //10
+console.log(binary_search(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])); //0
+console.log(binary_search(21, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]));//-1
+console.log(binary_search(21, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20]));//-1
+console.log(binary_search(12, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20]));//-1
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
