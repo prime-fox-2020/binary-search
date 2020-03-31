1c35821f1a66864a372310b46fdc6e70cff38d2f
Xaber <getar.metal@gmail.com>
2020-03-31 22:11:39 +0700

    binary-search first commit

--- index.js
+++ index.js
@@ -5,24 +5,51 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+    var max = arr.length;
+    for (let a = 1; a < max; a++) {
+      var min = arr[a];
+      var b = a - 1;
+      while (b >= 0 && arr[b] > min) {
+        arr[b + 1] = arr[b];
+        b = b - 1;
+      }
+      arr[b + 1] = min;
+    }
+    return arr;
 }
 
 function binarySearch (search, array) {
   // Your searching code
-  return 0;
+  var min = 0;
+  var max = array.length - 1;
+  while (min != max) {
+    var mid = Math.floor((min + max) / 2);
+    if (search < array[mid]) {
+      max = mid - 1;
+    } else {
+      min = mid;
+    }
+
+    if (array[min] == search) {
+      return min;
+    } else if (array[max] == search) {
+      return max;
+    }
+  }
+
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
 
-module.exports = binary_search
+module.exports = binarySearch;
