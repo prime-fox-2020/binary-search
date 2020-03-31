d1963e1362fe4956faa202d7d3b954df51b42d1f
chabode <rizqa.aulia@gmail.com>
2020-03-31 22:06:43 +0700

    solve binary search

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  var length = arr.length
+  for (var i = 1; i<length; i++){
+      var temp = arr[i]
+      var j = i - 1
+      while (j >= 0 && arr[j] > temp){
+          arr[j+1] = arr[j]
+          j = j-1
+      }
+      arr[j+1] = temp
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var low = 0
+  var high = array.length - 1
+  while (low <= high){
+      var mid = Math.floor((low + high)/2)
+      if (array[mid] == search){
+          return mid
+      } else if (array[mid] < search){
+          low = mid + 1
+      } else {
+          high = mid - 1
+      }
+  }
+  return -1
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
