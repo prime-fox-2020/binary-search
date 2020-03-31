181e0b768f845895c0ddb1a0e88fa03a820a7f07
pippop21 <afifar38@gmail.com>
2020-03-31 15:43:16 +0700

    add insertion Sort and binary search to index.js

--- index.js
+++ index.js
@@ -5,17 +5,38 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < i; j++){
+      if(arr[i] < arr[j]){
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
+  let high = array.length-1,
+      low  = 0,
+      mid  = Math.floor((high+low)/2);
+  while(low <= high && array[mid] !== search){
+    if(search > array[mid]) low = mid+1;
+    else high = mid-1;
+    mid = Math.floor((high+low)/2);
+    // console.log("high: " + high)
+    // console.log(low)
+  }
+  if(search === array[mid]) return true;
   return 0;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-
+console.log(arrayGenapSorted);
+console.log(arrayGanjilSorted);
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
