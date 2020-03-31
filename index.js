a0d2e668a11d04042fc1b7ed1d6e699c182a2445
aireljordan98 <joeyjordanwilliams@gmail.com>
2020-03-31 21:58:43 +0700

    airell

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-'use strict'
-
+'use strict'          
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+   
+  
+
   return arr
 }
 
-function binarySearch (search, array) {
+
+function binary_search(search, array) {
   // Your searching code
-  return 0;
-}
+  let bottom = 0;
+  let top = array.length-1
+  while (bottom <= top) {
+  let mid = Math.floor((top + bottom) / 2)
+  if (search == array[mid]) {
+  return mid
+  } else if (search > array[mid]) {
+  bottom = mid + 1
+  } else if (search < array[mid]) {
+  top = mid - 1
+  }
+  }
+  return -1
+  }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
