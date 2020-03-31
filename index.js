427be20156d249d7470a4b29e11d080b65fca270
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-03-31 18:45:43 +0700

    Fixed

--- index.js
+++ index.js
@@ -5,17 +5,52 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  //Insertion Sort
+   for (let i = 1; i < arr.length; i++) {
+    var temp = arr[i]
+    for (let j = i - 1; j >= 0; j--) {
+      if (temp > arr[j]) {
+        arr[j + 1] = temp
+        break
+      } else {
+        arr[j + 1] = arr[j]
+      }
+      if (j == 0) {
+        arr[0] = temp
+      }
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search(search, array) {
   // Your searching code
-  return 0;
+  let startIndex=0, lastIndex=array.length-1 
+  
+  while(startIndex<=lastIndex ){
+    let middleIndex=Math.floor((startIndex+lastIndex)/2)
+
+    if(search==array[middleIndex]){
+      return middleIndex
+    }
+    if(search<array[middleIndex]){
+      lastIndex=middleIndex - 1
+
+    }else if(search>array[middleIndex]){
+      startIndex=middleIndex + 1
+      
+    }
+    
+  }
+  return -1
+  // return 0;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
+
+// console.log(arrayGenapSorted,arrayGanjilSorted)
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
