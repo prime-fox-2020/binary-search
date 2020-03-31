baa944aeff295e5cbf10cf2d22adc6d787c98abb
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-03-31 20:42:15 +0700

    finished file index.js

--- index.js
+++ index.js
@@ -4,13 +4,40 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  let n = arr.length;
+  for (let i = 1; i < n; i++) {
+    let selected = arr[i];
+    let j = i -1;
+
+    while (j >= 0 && selected < arr[j]) {
+      arr[j+1] = arr[j];
+      j = j - 1;
+    }
+    arr[j+1] = selected;
+
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  let min = 0;
+  let max = array.length -1;
+
+  while (min <= max) {
+      let med = Math.floor((max + min)/2);
+
+      if (array[med]=== search) {
+        return med;
+      }
+      else if (array[med]< search) {
+        min = med + 1;
+      }
+      else {
+        max = med -1;
+      }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -21,6 +48,7 @@ console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
 
+console.log(arrayGanjilSorted);
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
