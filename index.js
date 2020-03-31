7877aa3b5b365464fe2b4955f6fb7198a6a67a1f
michaelm96 <michaelarceus96@gmail.com>
2020-03-31 21:55:32 +0700

    fixing and add pseudocode

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 'use strict'
 
+//Pseudocode
+//SET function that take array as parameter
+// FOR loop length of arr
+//  INIT j to index i-1
+//  INIT temp to arr index i
+//  WHILE j Greater Than or equal to 0 AND arr index j Greater than temp
+//    arr index j plus 1 is to arr index j Then
+//    DECREMENT J
+//  ENDWHILE
+//  arr index j plus 1 is temp
+// ENDFOR
+// DISPLAY arr
+
+
+// SET Function binarySearch that takes 2 parameters
+//  INIT awal to 0
+//  INIT akhir to length of parameter array minus 1
+//    WHILE awal less than or equal to akhir then
+//      INIT tengah is floor of (awal plus akhir) div by 2
+//      IF array index tengah less than search (parameter) Then
+//        awal is tengah plus 1
+//      ELSE array index tengah greater than search Then
+//        akhir is tengah minus 1
+//      ELSE array index tengah is equal to search then
+//        Display tengah
+//      ENDIF
+//    ENDWHILE
+//  DISPLAY -1
+
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
@@ -28,10 +57,10 @@ function binarySearch (search, array) {
       }else if(array[tengah] > search){
         akhir = tengah - 1
       }else if(array[tengah] == search){
-        return true
+        return tengah
       }
     }
-  return false;
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
