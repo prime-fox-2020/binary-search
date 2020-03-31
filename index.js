3860379afc0d6339aab4ce8dc88a456edf1c0a24
Adib Faqih <foezst@gmail.com>
2020-03-31 21:18:26 +0700

    solved, release 0

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
+  var idx = 0;
+  for (var i = 0; i < arr.length; i++) {
+    idx = i;
+    for (var j = i; j >= 0; j--) {
+      if (arr[idx] < arr[j]) {
+        var temp = arr[j];
+        arr[j] = arr[idx];
+        arr[idx] = temp;
+      } else {
+        idx = j;
+      }
+    }
+  }
   return arr
 }
 
 function binarySearch (search, array) {
-  // Your searching code
+  
   return 0;
 }
 
