81949f6001a03b3768ace4da297d32a59b19b887
zuhdi <zudhy.duddy@gmail.com>
2020-03-31 20:47:36 +0700

    done binary search

--- index.js
+++ index.js
@@ -5,12 +5,38 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]){
+        var swap = arr[i]
+        arr[i] = arr[j]
+        arr[j] = swap
+      }
+    }
+  }
+  console.log(arr);
+  
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var a = 0
+  var b = array.length - 1
+  
+  while (a <= b){
+    var temp = Math.floor((a + b) / 2)
+    if (array[temp] == search){
+      return temp
+      
+    } else if (array[temp] < search){
+      a = temp + 1
+    } else {
+      b = temp -1
+    }
+  }
+  
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
