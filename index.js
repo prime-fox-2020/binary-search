5cceba4d53864867c1f3881917188983da430371
alamrumambi <alam.abumaryam@gmail.com>
2020-03-31 08:27:33 -0400

    binary-search

--- index.js
+++ index.js
@@ -4,13 +4,35 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
+  for (let i = 0; i < arr.length; i++){
+    let sml = arr[i];
+    let smlInd = i;
+    for (let j = i + 1; j < arr.length; j++){
+      if (arr[j] < sml){
+        sml = arr[j];
+        smlInd = j;
+      }
+    }
+    arr[smlInd] = arr[i];
+    arr[i] = sml;
+  }
   // Your sorting code
   return arr
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
+  let mid = 0, bigInd = array.length - 1, smlInd = 0;
+  while (bigInd !== smlInd){
+    mid = Math.floor((smlInd + bigInd) / 2);
+    if (array[mid] == search) return mid;
+    else if (array[mid] > search) bigInd = mid;
+    else {
+      if (mid == smlInd) break;
+      smlInd = mid;
+    } 
+  }
   // Your searching code
-  return 0;
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
