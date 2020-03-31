1dfd5d97e0aad2ac45a0d3294d59516672ff5a9e
riolly <albert.r.cahyantara@gmail.com>
2020-03-31 20:15:12 +0700

    Release 0: Insertion sort

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  for (let i = 0; i < arr.length - 1; i++) {
+    let inspected = arr[i + 1];
+    for (let j = i; j >= 0; j--) {
+      if (arr[j] > inspected) {
+        arr[j + 1] = arr[j];
+        arr[j] = inspected;
+      } else {
+        break;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  return -1;
 }
 
-var arrayGenapSorted = ownSort(testArrayGenap)
-var arrayGanjilSorted = ownSort(testArrayGanjil)
+var arrayGenapSorted = ownSort(testArrayGenap);
+var arrayGanjilSorted = ownSort(testArrayGanjil);
+console.log(arrayGenapSorted);
+console.log(arrayGanjilSorted);
 
-// Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+// // Driver code
+// console.log(arrayGenapSorted);
+// console.log(binary_search(8, arrayGenapSorted))
+// console.log(binary_search(10, arrayGenapSorted))
+// console.log(binary_search(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+// console.log(arrayGanjilSorted);
+// console.log(binary_search(53, arrayGanjilSorted))
+// console.log(binary_search(3, arrayGanjilSorted))
+// console.log(binary_search(2, arrayGanjilSorted))
 
 module.exports = binary_search
