806568b6034b1345f6df7a5f8cf2b4905aa4f526
susilo hartomo <encusilo545110@gmail.com>
2020-03-31 15:58:14 +0700

    release 0

--- index.js
+++ index.js
@@ -5,10 +5,29 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+
+  //INSERTION SORT
+  console.table([arr]);
+  for (let i = 1; i < arr.length; i++) {
+    console.log('i: ', i, " ", arr[i]);
+    let temp1 = arr[i]
+    let tempIdx = i
+    for (let j = i - 1; j >= 0; j--) {
+      if (temp1 < arr[j]) {
+        let temp = arr[tempIdx]
+        arr[tempIdx] = arr[j]
+        arr[j] = temp
+        tempIdx = j
+      } else {
+        break
+      }
+      console.table([arr]);
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+function binarySearch(search, array) {
   // Your searching code
   return 0;
 }
