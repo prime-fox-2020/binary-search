4b696e4d61b79b33970cb7578c371829e2b8d14e
pippop21 <afifar38@gmail.com>
2020-03-31 17:45:20 +0700

    Release 1, add pseudoCode and index of number searched

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+/* 
+=========PSEUDOCODE===============
+//INSERTION SORT
+START FUNCTION ownSort WITH PARAMETER arr
+  FOR ITERATION BY i ON arr
+    FOR ITERATION BY j EQUAL 0 TO i
+      IF arr INDEX OF i LESS THAN arr INDEX OF j THEN
+        SET temp EQUAL TO arr INDEX OF i
+        arr INDEX OF i EQUAL TO arr INDEX OF j
+        arr INDEX OF j EQUAL TO temp
+      END IF
+    END FOR
+  END FOR
+  DISPLAY arr
+END FUNCTION
+
+
+//BINARY SEARCH
+START FUNCTION binary_search WITH PARAMETER OF search AND array
+  SET high EQUAL TO LENGTH OF array MINUS 1
+  SET low  EQUAL TO 0
+  SET mid  EQUAL TO HALF OF high PLUS low DIVIDED BY 2
+  WHILE low LESS THAN EUAL high AND array INDEX mid NOT EQUAL TO search
+    IF search BIGGER THAN array INDEX OF mid THEN low EQUAL TO mid PLUS 1
+    ELSE high EQUAL TO mid MINUS 1
+    END IF
+    mid  EQUAL TO HALF OF high PLUS low DIVIDED BY 2
+  END WHILE
+  IF search EQUAL TO array INDEX OF mid THEN DISPLAY mid
+  ELSE DISPLAY -1;
+  END IF
+END FUNCTION
+}
+*/
+
+
 'use strict'
 
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
@@ -26,17 +62,13 @@ function binary_search (search, array) {
     if(search > array[mid]) low = mid+1;
     else high = mid-1;
     mid = Math.floor((high+low)/2);
-    // console.log("high: " + high)
-    // console.log(low)
   }
-  if(search === array[mid]) return true;
-  return 0;
+  if(search === array[mid]) return mid;
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
-console.log(arrayGenapSorted);
-console.log(arrayGanjilSorted);
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
